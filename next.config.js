1b1e562d6a7b976daa5d9210ef5694f0a90d8e51
aheart500 <aheart500@yahoo.com>
2024-03-28 07:15:30 +0200

    update webpack

--- next.config.js
+++ next.config.js
@@ -1,12 +1,2 @@
 const withImages = require("next-images");
-module.exports = withImages({
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /@apollo\/server/,
-        use: "null-loader",
-      });
-    }
-    return config;
-  },
-});
+module.exports = withImages();
