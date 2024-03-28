7fdeab4a263f8eeb6cfe1438536d4c37fb80630a
aheart500 <aheart500@yahoo.com>
2024-03-28 06:50:16 +0200

    apolloServer Loader

--- next.config.js
+++ next.config.js
@@ -1,2 +1,22 @@
-const withImages = require('next-images')
-module.exports = withImages()
+const withImages = require("next-images");
+
+module.exports = withImages({
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        include: /node_modules\/@apollo\/server/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-class-properties"],
+            },
+          },
+        ],
+      });
+    }
+    return config;
+  },
+});
