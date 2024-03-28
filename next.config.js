69cf9b1ac115750264d135e907f10c834e0bf72c
aheart500 <aheart500@yahoo.com>
2024-03-28 07:10:35 +0200

    apolloServer Loader

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const withImages = require("next-images");
-module.exports = withImages();
+module.exports = withImages({
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /@apollo\/server/,
+        use: "null-loader",
+      });
+    }
+    return config;
+  },
+});
